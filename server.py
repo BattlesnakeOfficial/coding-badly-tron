59423d3f9d3a15729b299de74ea4203c6aafef62
bvanvugt <1531419+bvanvugt@users.noreply.github.com>
2020-11-29 10:45:27 -0800

    Refactor some stuff.

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import os
-import random
 
 import cherrypy
 
@@ -15,9 +14,6 @@ class Server(object):
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def index(self):
-        # This function is called when you register your Battlesnake on play.battlesnake.com
-        # It controls your Battlesnake appearance and author permissions.
-        # TIP: If you open your Battlesnake URL in browser you should see this data
         snake = Battlesnake()
         return {
             "apiversion": snake.apiversion,
@@ -31,11 +27,6 @@ class Server(object):
     @cherrypy.expose
     @cherrypy.tools.json_in()
     def start(self):
-        # This function is called everytime your snake is entered into a game.
-        # cherrypy.request.json contains information about the game that's about to be played.
-        # TODO: Use this function to decide how your snake is going to look on the board.
-        data = cherrypy.request.json
-
         print("START")
         return "ok"
 
@@ -43,9 +34,6 @@ class Server(object):
     @cherrypy.tools.json_in()
     @cherrypy.tools.json_out()
     def move(self):
-        # This function is called on every turn of a game. It's how your snake decides where to move.
-        # Valid moves are "up", "down", "left", or "right".
-        # TODO: Use the information in cherrypy.request.json to decide your next move.
         data = cherrypy.request.json
         move = Battlesnake().move(data)
         return {"move": move}
@@ -53,10 +41,6 @@ class Server(object):
     @cherrypy.expose
     @cherrypy.tools.json_in()
     def end(self):
-        # This function is called when a game your snake was in ends.
-        # It's purely for informational purposes, you don't have to make any decisions here.
-        data = cherrypy.request.json
-
         print("END")
         return "ok"
 
